6bb2ab4796beef8e6955c0491eac0a3496c35cdb
Adam <adamorlowskipoland@icloud.com>
2019-05-11 14:52:23 +0200

    update default-values for real ones, delete postalCodes from store

--- src/store.js
+++ src/store.js
@@ -6,13 +6,11 @@ import { saveStatePlugin } from './utils';
 Vue.use(Vuex);
 
 const stations = JSON.parse(localStorage.getItem('stations')) || defaultStations;
-const postalCodes = JSON.parse(localStorage.getItem('postalCodes')) || [];
 
 export default new Vuex.Store({
   plugins: [saveStatePlugin],
   state: {
     stations,
-    postalCodes,
   },
   mutations: {
 
