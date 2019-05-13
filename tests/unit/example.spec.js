747cc2bd41b5e0eec0ae401dbf91c7f53c105c8c
Adam <adamorlowskipoland@icloud.com>
2019-05-14 00:53:27 +0200

    add unit test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,23 @@
 import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/BaseInput.vue';
+import BaseButton from '@/components/BaseButton.vue';
 
-describe('HelloWorld.vue', () => {
+describe.skip('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
+  });
+});
+
+describe('BaseButton.vue', () => {
+  it('renders props.type when passed', () => {
+    const type = 'danger';
+    const wrapper = shallowMount(BaseButton, {
+      propsData: { type },
+    });
+    expect(wrapper.classes()).toContain('bg-red');
   });
 });
